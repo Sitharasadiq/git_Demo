b0455f9d7c709b42c30dd2ed997086082811e350
sithara <sitharasadiq@gmail.com>
2025-09-26 09:50:00 -0400

    changed controller

--- src/main/java/com/example/myProjectDemo/controller/HelloController.java
+++ src/main/java/com/example/myProjectDemo/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, from numpy ninja!";
     }
 
 }
