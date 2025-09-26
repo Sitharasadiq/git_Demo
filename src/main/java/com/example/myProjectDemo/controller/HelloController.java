ec906f1603eb2ca341f31978f97a93c36b1e468b
sithara <sitharasadiq@gmail.com>
2025-09-26 15:41:35 -0400

    second commit

--- src/main/java/com/example/myProjectDemo/controller/HelloController.java
+++ src/main/java/com/example/myProjectDemo/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public String sayHello() {
-        return "Hello, from numpy ninja!";
+        return "Hello, from numpy ninjadbg!";
     }
 
 }
